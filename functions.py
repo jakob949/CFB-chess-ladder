3e69ce46356b6b41bb7e2919bc9892521d7c0833
N. Jakob Larsen <98743287+jakob949@users.noreply.github.com>
2024-03-01 10:32:46 +0100

    Update functions.py

--- functions.py
+++ functions.py
@@ -1,4 +1,7 @@
 def add_1_ID(s_id):
     return str(int(s_id) + 1).zfill(len(s_id))
 
-print(add_1_ID('0999'))
+def delete_player(player_to_remove, ladder):
+    player_to_remove = next((player for player in ladder.players if player.name == player_to_remove), None)
+    if player_to_remove is not None:
+        ladder.remove_player(player_to_remove)
