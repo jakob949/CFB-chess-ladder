be24a163ae227ad6ac35286733a467a03f48a38e
jabob <jakob949@hotmail.com>
2024-03-05 17:17:08 +0100

    Displaying last games

---
+++ mail.py
@@ -0,0 +1,6 @@
+app.config['MAIL_SERVER'] = 'smtp.gmail.com'
+app.config['MAIL_PORT'] = 587
+app.config['MAIL_USE_TLS'] = True
+app.config['MAIL_USERNAME'] = 'jakob.larsen949@gmail.com'
+app.config['MAIL_PASSWORD'] = 'ihbt srdf ezyx phyr '
+app.config['MAIL_DEFAULT_SENDER'] = 'jakob.larsen949@gmail.com'
